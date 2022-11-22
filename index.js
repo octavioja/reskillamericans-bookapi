e14109e6d6195b9b31f30cd03b29759e33939af5
Octavio jamison <omunguia96@icloud.com>
2022-11-21 19:07:28 -0800

    Correctio on line 19

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 
 
-const http = require('module');
+const http = require('http');
 
 
-const books = ['the alchemist', 'absalom, absalom', 'time to kill', 'the great gatsby'];   // array of books
-const server = http.Module((req, res) => {
+
+const books = "['the alchemist', 'absalom, absalom', 'time to kill', 'the great gatsby']";   // array of books
+const server = http.createServer((req, res) => {
 const endpoint = req.url;
 if (endpoint === '/') { 
  res.end ('Welcome to the Bookstore');    
@@ -14,13 +15,11 @@ if (endpoint === '/') {
     res.end ('Contact us at: 123-456-7890');
 } else if (endpoint === '/about') {
     res.end ('We are a bookstore');
-} else if (endpoint === '/books') 
-  books.forEach(element => res.end(element));
-else {
-    // 404 page not found
-    res.end ('Page not found');
+} else if (endpoint === '/books') {
+  return res.end(books); // return the array of books
 }
 });
+
 
 
 
@@ -38,3 +37,8 @@ const hostname = 'localhost';
 server.listen(port, hostname, () => {
     console.log('Server is up and running');
   });
+
+  ///port=8989
+//hostname=localhost
+//http module
+//Endpoints: Page Not Found, Home Page, All books (Fetch all books from an array)
